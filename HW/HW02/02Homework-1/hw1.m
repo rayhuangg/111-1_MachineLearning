3f1039a2a753552d258e167ff1338b916875cad8
“Ray” <ray00211@gmail.com>
2022-09-25 18:45:13 +0800

    加入圖片title 並排

--- HW/HW02/02Homework-1/hw1.m
+++ HW/HW02/02Homework-1/hw1.m
@@ -2,14 +2,23 @@ clc; clear;
 
 X = load('02HW1_Xtrain');
 Y = load('02HW1_Ytrain');
-plot(X,Y, "o");
-hold on;
+f = figure;  
+f.Position = [10 50 550 400]; 
 
-p = polyfit(X ,Y, 2);
-y1 = polyval(p, X);
+for n=1:3
+    subplot(1, 3, n)
+    plot(X, Y, "o");
+    hold on;
+    
+    p = polyfit(X ,Y, n);
+    x1 = linspace(0, 1, 20);
+    y1 = polyval(p, x1);
+    
+    plot(x1, y1);
+    legend('data','fit')
+    title(' order: ',n)
+    hold off;
 
-plot(X, y1, '-')
-hold off;
-
+end
 
 % plot(X,Y,'o',X,y1,'-')
